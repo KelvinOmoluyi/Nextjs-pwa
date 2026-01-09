60cafd0ea03a7e0e2a74943261a63a6ccd042028
Kelvin Omoluyi <omoluyikelvin14@gmail.com>
2026-01-09 21:26:01 +0100

    (fix): modified web app details in manifest file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Real Estate</h1>
+      <div>
+        <h1>Real Estate</h1>
+
+        <Link href="/houses">
+          <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
+            link
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
